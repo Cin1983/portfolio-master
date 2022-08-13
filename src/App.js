21d5266f41bc3fc8b3a0835d3af1f5d078544712
Maria <cinnymaria@gmail.com>
2022-08-13 09:58:54 +0200

    css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
-
-function App() {
-  return (
-    <div>App</div>
-  )
+const slide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('navbar');
+    const navlinks = document.querySelectorAll('.links li');
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('navbaract')
+        navlinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navlinkfade 4s ease backwards ${index / 10}s`;
+                //       }
+                //     });
+                // });
+            }
+        });
+    })
 }
-
-export default App
+  slide()
